724270803c3d3d8308fda435e31bd092862c9e9f
Serena Nam <serena.nam@nyu.edu>
2024-12-02 01:59:54 -0500

    final website files

---
+++ script.js
@@ -0,0 +1,54 @@
+let startButton = document.getElementById("startButton");
+let executeButton = document.getElementById("executeButton");
+let titleInput = document.getElementById("title");
+let artistInput = document.getElementById("artist");
+let question = document.getElementById("questionText");
+let answer = document.getElementById("answerText");
+let playAgainButton = document.getElementById("playAgainButton");
+
+let songs = ["🌊👀", "🗣🌝", "🍉🍬", "🕊️🪶", "🪦😄", "🙏🙏🙏", "🏢"];
+let titles = ["Ocean Eyes", "Talking to the Moon", "Watermelon Sugar", "Birds of a Feather", "Die with a Smile", "Please Please Please", "Apt"];
+let artists = ["Billie Eilish", "Bruno Mars", "Harry Styles", "Billie Eilish", "Bruno Mars, Lady Gaga", "Sabrina Carpenter", "Rose, Bruno Mars"];
+
+let index = 0;
+console.log(window.location.pathname); 
+console.log(songs[index]);
+
+if(window.location.pathname.includes("index.html")) {
+	startButton.addEventListener("click", function() {
+		console.log("Start Button Clicked!");
+		window.location.href = "game.html";
+		
+	});
+}
+
+console.log(index);
+if(window.location.pathname.includes("game.html")) {
+	generateQuestion();
+	executeButton.addEventListener("click", function() {
+		let title = titleInput.value;
+		let artist = artistInput.value;
+		checkAnswer();
+	});
+
+	playAgainButton.addEventListener("click", function() {
+    	window.location.reload(); // This will reload the current page
+	});
+}
+
+
+function generateQuestion() {
+	index = Math.floor(Math.random() * songs.length);
+	console.log(songs[index]);
+	question.innerText = songs[index];
+}
+
+function checkAnswer() {
+	if(titleInput.value.toLowerCase() == titles[index].toLowerCase() && artistInput.value.toLowerCase() == artists[index].toLowerCase()) {
+		answer.innerText = "Correct!";
+	} else {
+		answer.innerText = "Incorrect! It's actually " + titles[index] + " by " + artists[index];
+	}
+
+	playAgainButton.style.display = "inline-block";
+}
